d39261843dd8aaafe67ce6ef485b1d8fbdd6121c
Dung Nguyen <dungnguyen9599@gmail.com>
2020-10-21 13:53:42 +0700

    add config

--- hicomfbackend/settings.py
+++ hicomfbackend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '$wat)utj+58h078_a$0$h!1*%tkc9ub1rdgobtf^^fqm00sqq('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
